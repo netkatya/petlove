071c4cea3852175590edb019731384009b859426
Ievgen Prygoda <e.klimochenkova@gmail.com>
2026-02-08 22:35:35 +0000

    sizes fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,39 +6,42 @@ export default function LogIn() {
   return (
     <main className="pt-20 md:pt-28.5 pb-5 md:pb-8">
       <div className="container grid grid-cols-1 xl:grid-cols-2 gap-2.5 lg:gap-8">
-        <picture className="block w-full">
-          {/* Desktop */}
-          <source
-            srcSet="
+        <div className="rounded-[30px] md:rounded-[60px] overflow-hidden">
+          <picture className="block w-full h-full">
+            {/* Desktop */}
+            <source
+              srcSet="
           /img/login/login-desc@1x.webp 1x,
           /img/login/login-desc@2x.webp 2x
         "
-            media="(min-width: 1280px)"
-          />
+              media="(min-width: 1280px)"
+            />
 
-          {/* Tablet */}
-          <source
-            srcSet="
+            {/* Tablet */}
+            <source
+              srcSet="
           /img/login/login-tab@1x.webp 1x,
           /img/login/login-tab@2x.webp 2x
         "
-            media="(min-width: 768px)"
-          />
+              media="(min-width: 768px)"
+            />
 
-          {/* Mobile */}
-          <source
-            srcSet="
+            {/* Mobile */}
+            <source
+              srcSet="
           /img/login/login-mob@1x.webp 1x,
           /img/login/login-mob@1x.webp 2x
         "
-            src="/img/login/login-mob@1x.webp"
-          />
-          <img
-            src="/img/login/login-mob@1x.webp"
-            alt="Dog"
-            className="object-contain w-full h-auto"
-          />
-        </picture>
+              src="/img/login/login-mob@1x.webp"
+            />
+            <img
+              src="/img/login/login-mob@1x.webp"
+              alt="Dog"
+              className="object-cover w-full h-full"
+            />
+          </picture>
+        </div>
+
         <div className="flex flex-col rounded-[30px] md:rounded-[60px] bg-(--light-text) px-5 md:px-35 xl:px-21 py-15 md:py-17.75 xl:py-29.5">
           <PageTitle>Log in</PageTitle>
           <p className="mt-4 md:mt-8 mb-6 md:mb-8 font-medium text-[14px] md:text-[18px] leading-[129%] md:leading-[122%] tracking-[-0.02em]">
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,41 +5,44 @@ import PageTitle from "@/components/PageTitle";
 export default function RegisterPage() {
   return (
     <main className="pt-20 md:pt-28.5 pb-5 md:pb-8">
-      <div className="container grid grid-cols-1 xl:grid-cols-2 gap-2.5 lg:gap-8">
-        <picture className="block w-full">
-          {/* Desktop */}
-          <source
-            srcSet="
+      <div className="container grid grid-cols-1 xl:grid-cols-2 gap-2.5 xl:gap-8 items-stretch">
+        <div className="rounded-[30px] md:rounded-[60px] overflow-hidden">
+          <picture className="block w-full h-full">
+            {/* Desktop */}
+            <source
+              srcSet="
           /img/registration/reg-desc@1x.webp 1x,
           /img/registration/reg-desc@2x.webp 2x
         "
-            media="(min-width: 1280px)"
-          />
+              media="(min-width: 1280px)"
+            />
 
-          {/* Tablet */}
-          <source
-            srcSet="
+            {/* Tablet */}
+            <source
+              srcSet="
           /img/registration/reg-tablet@1x.webp 1x,
           /img/registration/reg-tablet@2x.webp 2x
         "
-            media="(min-width: 768px)"
-          />
+              media="(min-width: 768px)"
+            />
 
-          {/* Mobile */}
-          <source
-            srcSet="
+            {/* Mobile */}
+            <source
+              srcSet="
           /img/registration/reg-mob@1x.webp 1x,
           /img/registration/reg-mob@1x.webp 2x
         "
-            src="/img/registration/reg-mob@1x.webp"
-          />
-          <img
-            src="/img/registration/reg-mob@1x.webp"
-            alt="Cat"
-            className="object-contain w-full h-auto"
-          />
-        </picture>
-        <div className="flex flex-col rounded-[30px] md:rounded-[60px] bg-(--light-text) px-5 md:px-35 xl:px-21 py-6.75 md:py-7.5 xl:py-19.25">
+              src="/img/registration/reg-mob@1x.webp"
+            />
+            <img
+              src="/img/registration/reg-mob@1x.webp"
+              alt="Cat"
+              className="object-cover w-full h-full"
+            />
+          </picture>
+        </div>
+
+        <div className="flex flex-col h-full rounded-[30px] md:rounded-[60px] bg-(--light-text) px-5 md:px-35 xl:px-21 py-6.75 md:py-7.5 xl:py-19.25">
           <PageTitle>Registration</PageTitle>
           <p className="mt-3 md:mt-4 mb-5 md:mb-8 font-medium text-[14px] md:text-[18px] leading-[129%] md:leading-[122%] tracking-[-0.02em]">
             Thank you for your interest in our platform.
